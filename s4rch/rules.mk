4138903dd66963564997458eaa0c1b8280f4897d
Sergio Ribera <56278796+SergioRibera@users.noreply.github.com>
2025-05-16 23:50:14 -0400

    build: clear rules and add via

--- s4rch/rules.mk
+++ s4rch/rules.mk
@@ -7,15 +7,15 @@ BLUETOOTH_ENABLE    	= no      	 	# Enable Bluetooth with the Adafruit EZ-Key HI
 SWAP_HANDS_ENABLE   	= no      	 	# Enable one-hand typing
 MOUSEKEY_ENABLE     	= no      	 	# Disable mouse control by keyboard
 EXTRAKERY_ENABLE    	= no      	 	#
-VIA_ENABLE          	= no      	 	# Enable Modify Layout on Runtime
+VIA_ENABLE          	= yes      	 	# Enable Modify Layout on Runtime
 
 RGBLIGHT_ENABLE     	= no 			# Enable WS2812 RGB underlight.
-CONSOLE_ENABLE  	= yes
+# CONSOLE_ENABLE  	    = yes
 RGB_MATRIX_ENABLE   	= yes
-RGB_MATRIX_DRIVER       = WS2812
-RAW_ENABLE 	    	= yes     		# Allow cominucate with host
+# RGB_MATRIX_DRIVER     = WS2812
+# RAW_ENABLE 	    	= yes     		# Allow cominucate with host
 TAP_DANCE_ENABLE    	= yes     		# Enable Double tap for send other keycode
 OLED_ENABLE         	= no 			# Enable OLED
-# OLED_DRIVER             = SSD1306
+# OLED_DRIVER           = SSD1306
 
 LTO_ENABLE              = yes 	      		# Compresion of the output firmware
